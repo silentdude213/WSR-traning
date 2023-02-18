10a2d9c2b3fb859821c22e8025de75f7e9ecbb0e
Kirill <epickplayer@yandex.ru>
2023-02-18 11:41:07 +0700

    добавление адаптивного слайдера

--- js/slider.js
+++ js/slider.js
@@ -1,12 +1,35 @@
+'use strict';
 
-/* 
-Переменная, хранящая позицию
-Сколько слайдов показываем
-Сколько слайдов пролистываем
-Переменная с контейнером
-Переменная с трэком
-Переменная с слайдами
-Счётчик количества слайдов
-Минимальная ширина слайдов
-Позиция перемещения = количество слайдов на минимальную ширину слайда
-*/ 
+let position = 0;
+const showSlide = 1;
+const scrollSlide = 1;
+const container = document.querySelector(".section-booking__slider");
+const track = document.querySelector('.slider__track');
+const slides = document.querySelectorAll('.slide');
+const slidesCount = slides.length;
+let slideWidth;
+let movePosition;
+
+function SetWidth(){
+    slideWidth = container.clientWidth;
+    movePosition = showSlide * slideWidth;
+    slides.forEach((slide) => {
+        slide.style.minWidth = `${slideWidth}px`;
+     });
+    SetPosition();
+};
+
+window.addEventListener("resize", SetWidth);
+SetWidth();
+
+let timer = setInterval(() => {
+    position -= movePosition;
+    if (position <= -(slidesCount - showSlide) * slideWidth){
+        position = 0;
+    }
+    SetPosition();
+}, 3000);
+
+function SetPosition(){
+    track.style.transform = `translateX(${position - movePosition}px)`;
+}
